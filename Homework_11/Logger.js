4bdd9b8011b4cc58094b67077d36673e6dfeea98
Radevac <radevac000@gmail.com>
2024-08-17 17:25:17 +0300

    Hom11
    Signed-off-by: Radevac <radevac000@gmail.com>

---
+++ Homework_11/Logger.js
@@ -0,0 +1,12 @@
+const Logger = {
+    info: (message) =>{
+        console.log(message)
+    },
+    error: (message) =>{
+        // fetch('http://api.ninydev.com/error/1/', {
+        //     method: 'POST',
+        //     body: JSON.stringify(message)
+        // })
+        console.error(message)
+    }
+}
---
+++ Homework_11/novaposhtaFetch.js
@@ -0,0 +1,49 @@
+const aptKey = '70c790dbca123facef86380d79edf525';
+
+function npFetchCallBack(calledMethod, methodProperties, callback) {
+
+    const cacheKey = {
+        calledMethod: calledMethod,
+        methodProperties: methodProperties
+    }
+
+    let cache = MyLocalStorage.get(cacheKey)
+
+
+    if (cache) {
+        myToastify('From Cache')
+        callback(cache)
+        return
+    }
+
+    fetch('https://api.novaposhta.ua/v2.0/json/', {
+        method: 'POST',
+        body: JSON.stringify({
+            "apiKey": aptKey,
+            "modelName": "AddressGeneral",
+            "calledMethod": calledMethod,
+            "methodProperties": methodProperties
+        })
+    })
+        .then(res => res.json())
+        .then(jsonResponse => {
+            Logger.info(jsonResponse)
+            if (!jsonResponse.success) {
+                throw  { message : 'Data Error '}
+            }
+
+            if(jsonResponse.info.totalCount)
+                myToastify("Get records:" + jsonResponse.info.totalCount)
+
+            MyLocalStorage.set(cacheKey, jsonResponse.data)
+            callback(jsonResponse.data)
+
+        })
+        .catch(err => {
+            myToastify("Error" + err.message, {
+                background: "linear-gradient(to right, #550000, #ff0000)"
+            })
+            Logger.error(err)
+        })
+
+}
